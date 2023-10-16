1ea119f128942ae41426afbaf883f774c28b0abf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-16 14:31:42 +0300

    Not using divs

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,8 @@
 const GoalItem = () => {
 	return (
-		<div>
-			<View style={styles.individualGoal}>
-				<Text style={{ color: 'white' }}>{itemData.item.text}</Text>
-			</View>
-		</div>
+		<View style={styles.individualGoal}>
+			<Text style={{ color: 'white' }}>{itemData.item.text}</Text>
+		</View>
 	)
 }
 
