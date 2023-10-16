21a43eb9fad27e8f3222611ac0f0b33020db2b0f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-16 11:17:58 +0300

    Added two blank views

--- App.js
+++ App.js
@@ -2,7 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View>
+      <View></View>
+      <View></View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
