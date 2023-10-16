c7f7fb616b70554ea808c582e6a88a3e18fb5dc6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-16 11:19:42 +0300

    Added a textInput compoinent and a button on the first inner view

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View>
+      <View>
+        <TextInput placeholder="Your Course Goal" />
+        <Button title="Add Goal" />
+      </View>
       <View></View>
-      <View></View>
+      <StatusBar style="auto" />
     </View>
   );
 }
