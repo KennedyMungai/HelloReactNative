673b41478177ec35016fb88cd999c10f65f7a8f6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-16 10:56:06 +0300

    Got rid of the text component

--- App.js
+++ App.js
@@ -2,11 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Sim Simma. Who's got the keys to my beamer</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
