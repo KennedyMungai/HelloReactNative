fa65cddd9f84e1f2f8edee5373283f810c7d2181
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-16 12:37:23 +0300

    Got rid of the flatlist styles

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { useState } from 'react'
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import {
+	Button,
+	FlatList,
+	StyleSheet,
+	Text,
+	TextInput,
+	View
+} from 'react-native'
 
 export default function App() {
 	const [enteredGoalText, setEnteredGoalText] = useState('')
